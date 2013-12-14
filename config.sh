e614fa623fa2b7749bc12574e7fa1a389786af39
Alex Gajewski <apagajewski@gmail.com>
2013-12-14 16:54:48 -0600

    First Build

---
+++ config.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$name]..."
+
+#======================================
+# SuSEconfig
+#--------------------------------------
+echo "** Running suseConfig..."
+suseConfig
+
+echo "** Running ldconfig..."
+/sbin/ldconfig
+
+#======================================
+# Setup default runlevel
+#--------------------------------------
+baseSetRunlevel 5
+
+#======================================
+# Add missing gpg keys to rpm
+#--------------------------------------
+suseImportBuildKey
+
+
+#======================================
+# Firewall Configuration
+#--------------------------------------
+echo '** Configuring firewall...'
+chkconfig SuSEfirewall2_init on
+chkconfig SuSEfirewall2_setup on
+
+# Enable sshd
+chkconfig sshd on
+
+#======================================
+# Sysconfig Update
+#--------------------------------------
+echo '** Update sysconfig entries...'
+baseUpdateSysConfig /etc/sysconfig/network/config FIREWALL yes
+baseUpdateSysConfig /etc/init.d/suse_studio_firstboot NETWORKMANAGER yes
+baseUpdateSysConfig /etc/sysconfig/SuSEfirewall2 FW_SERVICES_EXT_TCP 22\ 80\ 443
+baseUpdateSysConfig /etc/sysconfig/console CONSOLE_FONT lat9w-16.psfu
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER_AUTOLOGIN tux
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER gdm
+baseUpdateSysConfig /etc/sysconfig/windowmanager DEFAULT_WM gnome
+
+
---
+++ root/usr/share/themes/Default/README.md
@@ -0,0 +1,24 @@
+Blumix is a Mod of Numix, but clearer and bluer :P
+
+Description and stuff:
+http://rhoconlinux.wordpress.com/wp-admin/post.php?post=744&action=edit&message=6&postpost=v2
+
+### installation
+
+1) Extract the zip file to the themes directory i.e. `/usr/share/themes/`
+
+2) Apply the theme with Gnome-tweak, Ubuntu-tweak or Elementary-tweaks. 
+
+### Manual installation
+
+
+
+To set the theme in Gnome, run the following commands in Terminal,
+
+```
+gsettings set org.gnome.desktop.interface gtk-theme "Blumix" && gsettings set org.gnome.desktop.wm.preferences theme "Blumix"
+```
+
+
+More on Numix:  [Numix@GitHub](https://github.com/shimmerproject/Numix)
+License: GPL-3.0+
